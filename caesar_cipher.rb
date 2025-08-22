5e9252c3c10854a203b2c27742a8ff13efd0920d
JonathanMooreFT <jonathan.moore@firmatek.com>
2025-08-22 14:46:42 -0500

    User Input and Overflow
    Added in ability for user input and a check for overflow to make sure parameters are between 0 and 25 and that the returned values loop within the array.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,19 +1,34 @@
+print ("What is your note?: ")
+string = gets.chomp
+
+print ("What is your shift?: ")
+shift = gets.chomp.to_i
+
 def caesar_cipher string, shift
-	alphabet = []
-	shift_factor = ""
-	
-	cipher = 'a'.ord
+	upper_alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+	lower_alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+	shift_factor = 0
+	out_array = []
 
 	string.split('').each do |letter|
-		if letter =~ /[^a-zA-Z]/
-			alphabet.push letter.bytes.join('').to_i
+		index = nil
+		min = 0
+		max = 25
+		if upper_alphabet.include?(letter) || lower_alphabet.include?(letter)
+			index = upper_alphabet.index(letter) || lower_alphabet.index(letter)
 		else
-			shifted_string = letter.downcase.bytes.join('').to_i + shift
-			alphabet.push (shifted_string - cipher) % 26 + cipher
+			out_array.push(letter)
+			next
 		end
-	end
-	shift_factor = alphabet.pack 'C*'
-	shift_factor.capitalize
+		shift_factor = index + shift.clamp(min, max) - 26
+		if upper_alphabet.include?(letter)
+			out_array.push(upper_alphabet[shift_factor])
+		else
+			out_array.push(lower_alphabet[shift_factor])
+			next
+		end
+ 	end
+	out_array.join
 end
 
-puts caesar_cipher("What a string!", 5)
+puts caesar_cipher(string, shift)
